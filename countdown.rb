25f4b5d9f94a1bd6282e45517a42da6914cbbd97
lgm527 <laurellmccaffrey@gmail.com>
2019-05-31 10:58:30 -0400

    countdown to the new year

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(delay)
+  sleep(delay)
+  counter = delay
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
