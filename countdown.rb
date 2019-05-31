22162aa9b43b854c442bd8edd202c7f4f85fbf0b
Samir Jasarat <samirjasarat@gmail.com>
2019-05-30 22:21:55 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+    def countdown(x)
+        while x > 0
+          puts "#{x} SECOND(S)!"
+          sleep 1
+          x -= 1
+        end	end
+        return "HAPPY NEW YEAR!"
+        end
 end
+
+
